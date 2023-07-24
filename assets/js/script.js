fd969ffcc58b8f1f000236b895ebc714a8911b2c
AdrianaStoi <>
2023-07-24 20:31:00 +0000

    Add match check function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,37 @@ let flipCards = document.querySelectorAll('.flip-cards');
 // variables for checking card match
 let frstCard, scndCard;
 let flippedCard = false;
+let stickBoard = false;
 
 // Function and click event for creating flip card 
 function createFlip() {
+    if (stickBoard) return;
     this.classList.add('flipping');
 
     if (!flippedCard) {
         flippedCard = true;
-        frstCard;.this;
+        frstCard = this;
+        return;
+    }
+
+    scndCard = this;
+    flippedCard = false;
+    matchCheck();
+}
+
+function matchCheck(); {
+    let whenMatch = frstCard.dataset.imagecard === scndCard.dataset.imagecard;
+
+    if (whenMatch) {
+        activateMatch();
+    } else {
+        revertCards();
     }
 }
+
+
+
+
 
 flipCards.forEach(tile => tile.addEventListener('click', createFlip));
 
